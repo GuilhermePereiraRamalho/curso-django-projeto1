55d9f6ddefa660efc3d7df19fb4e61ba758ca9fd
Guilherme Ramalho <137944713+GuilhermePereiraRamalho@users.noreply.github.com>
2025-05-07 10:03:22 +0200

    Testando "No recipes found" quando receitas não existem

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -4,28 +4,35 @@ from recipes import views
 
 
 class RecipeViewsTest(TestCase):
-    def test_recipe_home_view_function_is_correct(self) -> None:
+    def test_recipe_home_view_function_is_correct(self):
         view = resolve(
             reverse("recipes:home")
         )
         self.assertIs(view.func, views.home)
 
-    def test_recipe_category_view_function_is_correct(self) -> None:
+    def test_recipe_category_view_function_is_correct(self):
         view = resolve(
             reverse("recipes:category", kwargs={"category_id": 1})
         )
         self.assertIs(view.func, views.category)
 
-    def test_recipe_detail_view_function_is_correct(self) -> None:
+    def test_recipe_detail_view_function_is_correct(self):
         view = resolve(
             reverse("recipes:recipe", kwargs={"id": 1})
         )
         self.assertIs(view.func, views.recipe)
 
-    def test_recipe_home_view_returns_status_code_200_ok(self) -> None:
+    def test_recipe_home_view_returns_status_code_200_ok(self):
         response = self.client.get(reverse("recipes:home"))
         self.assertEqual(response.status_code, 200)
 
-    def test_recipe_home_view_loads_correct_template(self) -> None:
+    def test_recipe_home_view_loads_correct_template(self):
         response = self.client.get(reverse("recipes:home"))
         self.assertTemplateUsed(response, "recipes/pages/home.html")
+
+    def test_recipe_home_template_shows_no_recipes_found_if_no_recipes(self):
+        response = self.client.get(reverse("recipes:home"))
+        self.assertIn(
+            "<h1>No Recipes found here 😢</h1>",
+            response.content.decode('utf-8')
+        )
