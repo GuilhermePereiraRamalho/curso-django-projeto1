e8a66d6a76b4173166dac5d1a5b15d6ab5f28ab7
Guilherme Ramalho <137944713+GuilhermePereiraRamalho@users.noreply.github.com>
2025-05-08 22:08:45 +0200

    Testando o branch (caminho) "is_published" True e False

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -38,18 +38,18 @@ class RecipeViewsTest(RecipeTestBase):
         response_context_recipes = response.context['recipes']
 
         self.assertIn('Recipe Title', content)
-        # self.assertIn('10 Minutos', content)
-        # self.assertIn('5 Porções', content)
-        # self.assertIn('café da manha', content)
         self.assertEqual(len(response_context_recipes), 1)
 
-    def test_recipe_category_template_load_recipes(self):
-        need_title = 'This is a category test'
-        self.make_recipe(title=need_title)
-        response = self.client.get(reverse("recipes:category", args=(1,)))
-        content = response.content.decode('utf-8')
+    def test_recipe_home_template_dont_load_recipes_not_published(self):
+        """Test recipe is_published=False"""
 
-        self.assertIn(need_title, content)
+        self.make_recipe(is_published=False)
+        response = self.client.get(reverse("recipes:home"))
+
+        self.assertIn(
+            "<h1>No Recipes found here 😢</h1>",
+            response.content.decode('utf-8')
+        )
 
     def test_recipe_category_view_function_is_correct(self):
         view = resolve(
@@ -62,6 +62,14 @@ class RecipeViewsTest(RecipeTestBase):
             reverse("recipes:category", kwargs={"category_id": 1000})
         )
         self.assertEqual(response.status_code, 404)
+
+    def test_recipe_category_template_load_recipes(self):
+        need_title = 'This is a category test'
+        self.make_recipe(title=need_title)
+        response = self.client.get(reverse("recipes:category", args=(1,)))
+        content = response.content.decode('utf-8')
+
+        self.assertIn(need_title, content)
 
     def test_recipe_detail_view_function_is_correct(self):
         view = resolve(
