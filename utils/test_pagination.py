3a66c4bc5540bd8b113712af25dadd4ec0e11962
Guilherme Ramalho <137944713+GuilhermePereiraRamalho@users.noreply.github.com>
2025-05-20 10:11:47 +0200

    Corrigindo o range final da paginação

--- utils/test_pagination.py
+++ utils/test_pagination.py
@@ -9,16 +9,16 @@ class PaginationTest(TestCase):
             page_range=list(range(1, 21)),
             qty_pages=4,
             current_page=1,
-        )
+        )['pagination']
         self.assertEqual([1, 2, 3, 4], pagination)
 
-    def test_first_page_range_is_static_if_current_page_is_less_than_middle_page(self):  # noqa: E501
+    def test_make_pagination_range_first_page_range_is_static_if_current_page_is_less_than_middle_page(self):  # noqa: E501
         # current_page = 1 qty_pages = 2 middle_page = 2
         pagination = make_pagination_range(
             page_range=list(range(1, 21)),
             qty_pages=4,
             current_page=1,
-        )
+        )['pagination']
         self.assertEqual([1, 2, 3, 4], pagination)
 
         # current_page = 2 qty_pages = 2 middle_page = 2
@@ -26,7 +26,7 @@ class PaginationTest(TestCase):
             page_range=list(range(1, 21)),
             qty_pages=4,
             current_page=2,
-        )
+        )['pagination']
         self.assertEqual([1, 2, 3, 4], pagination)
 
         # current_page = 3 qty_pages = 2 middle_page = 2
@@ -35,5 +35,61 @@ class PaginationTest(TestCase):
             page_range=list(range(1, 21)),
             qty_pages=4,
             current_page=3,
-        )
+        )['pagination']
         self.assertEqual([2, 3, 4, 5], pagination)
+
+    def test_make_pagination_range_make_sure_middle_ranges_are_correct(self):
+        # current_page = 10 qty_pages = 2 middle_page = 2
+        # HERE RANGE SHOULD CHANGE
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            qty_pages=4,
+            current_page=10
+        )['pagination']
+        self.assertEqual([9, 10, 11, 12], pagination)
+
+        # current_page = 12 qty_pages = 2 middle_page = 2
+        # HERE RANGE SHOULD CHANGE
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            qty_pages=4,
+            current_page=12
+        )['pagination']
+        self.assertEqual([11, 12, 13, 14], pagination)
+
+    def test_make_pagination_range_is_static_when_last_page_is_next(self):
+        # current_page = 18 qty_pages = 2 middle_page = 2
+        # HERE RANGE SHOULD CHANGE
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            qty_pages=4,
+            current_page=18
+        )['pagination']
+        self.assertEqual([17, 18, 19, 20], pagination)
+
+        # current_page = 19 qty_pages = 2 middle_page = 2
+        # HERE RANGE SHOULD CHANGE
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            qty_pages=4,
+            current_page=19
+        )['pagination']
+        self.assertEqual([17, 18, 19, 20], pagination)
+
+        # current_page = 20 qty_pages = 2 middle_page = 2
+        # HERE RANGE SHOULD CHANGE
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            qty_pages=4,
+            current_page=20
+        )['pagination']
+        self.assertEqual([17, 18, 19, 20], pagination)
+
+        # current_page = 21 qty_pages = 2 middle_page = 2
+        # HERE RANGE SHOULD CHANGE
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            qty_pages=4,
+            current_page=21
+        )['pagination']
+        self.assertEqual([17, 18, 19, 20], pagination)
