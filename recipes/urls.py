60beb4b89e4facf7318c5887151582c52738fdb7
Guilherme Ramalho <guilherme.p.ramalho@gmail.com>
2025-07-31 10:26:41 +0200

    Adicionando JSONResponse para a API que usa DetailView

--- recipes/urls.py
+++ recipes/urls.py
@@ -26,4 +26,9 @@ urlpatterns = [
         views.RecipeListViewHomeApi.as_view(),
         name='recipe_api_v1'
     ),
+    path(
+        'recipes/api/v1/<int:pk>/',
+        views.RecipeDetailAPI.as_view(),
+        name='recipe_api_v1_detail'
+    ),
 ]
