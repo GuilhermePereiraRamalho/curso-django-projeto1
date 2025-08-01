12878636148e89b63b867ec5bd008d143baaf50f
Guilherme Ramalho <137944713+GuilhermePereiraRamalho@users.noreply.github.com>
2025-08-01 09:44:50 +0200

    instalação da Django Debug Toolbar

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from debug_toolbar.toolbar import debug_toolbar_urls
 
 
 urlpatterns = [
@@ -12,3 +13,4 @@ urlpatterns = [
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += debug_toolbar_urls()
