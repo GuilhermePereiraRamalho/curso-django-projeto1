cb8a96ac42ef58cf8e333538cf14cb7c44f06187
Guilherme Ramalho <137944713+GuilhermePereiraRamalho@users.noreply.github.com>
2025-09-10 10:08:42 +0200

    Campos relacionados no Django Rest Framework

--- recipes/serializers.py
+++ recipes/serializers.py
@@ -1,5 +1,15 @@
 from rest_framework import serializers
 
+from django.contrib.auth.models import User
+
+# from tag.models import Tag
+
+
+class TagSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    name = serializers.CharField(max_length=255)
+    slug = serializers.SlugField()
+
 
 class RecipeSerializer(serializers.Serializer):
     id = serializers.IntegerField()
@@ -7,6 +17,18 @@ class RecipeSerializer(serializers.Serializer):
     description = serializers.CharField(max_length=165)
     public = serializers.BooleanField(source='is_published')
     preparation = serializers.SerializerMethodField()
+    category = serializers.StringRelatedField()
+    author = serializers.PrimaryKeyRelatedField(
+        queryset=User.objects.all()
+    )
+    # tags = serializers.PrimaryKeyRelatedField(
+    #     queryset=Tag.objects.all(),
+    #     many=True
+    # )
+    tag = TagSerializer(
+        many=True,
+        source='tags'
+    )
 
     def get_preparation(self, recipe):
         return f'{recipe.preparation_time} {recipe.preparation_time_unit}'
