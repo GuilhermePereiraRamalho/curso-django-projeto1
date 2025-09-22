77ba4213ba0a01f7258bfc887d3c8c2ca7f5662a
Guilherme Ramalho <137944713+GuilhermePereiraRamalho@users.noreply.github.com>
2025-09-22 09:42:26 +0200

    Adicionando configurações para o Simple JWT

--- config/settings/rest_framework.py
+++ config/settings/rest_framework.py
@@ -1,3 +1,8 @@
+from datetime import timedelta
+
+import os
+
+
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': (
         'rest_framework.pagination.LimitOffsetPagination'
@@ -6,4 +11,14 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+}
+
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ROTATE_REFRESH_TOKENS": False,
+    "BLACKLIST_AFTER_ROTATION": False,
+    "SIGNING_KEY": os.environ.get('SECRET_KEY_JWT', "INSECURE"),
+    "AUTH_HEADER_TYPES": ("Bearer",),
 }
